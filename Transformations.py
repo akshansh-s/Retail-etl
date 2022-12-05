f5421de2fbc1febfa5c8d0c3b82eb01408c5d25d
Akshansh <s.akshansh01@gmail.com>
2022-12-05 10:07:07 +0530

    Added joiner functionality

--- Transformations.py
+++ Transformations.py
@@ -29,6 +29,14 @@ def aggregate(group_choice):
     agg_gd=grouped_data.agg({'Sales_Amount':'mean'})
     print("\n",agg_gd)
 
+def joiner(file1,file2):
+    ff=pd.merge(file1,file2)
+    print(ff)
+    print("Want to save the merged table in an excel file?\n1. Yes\t2. No\n")
+    c=int(input())
+    if(c==1):
+        write_target(ff)
+
 if __name__ == "__main__":
     choice = 1
 
@@ -37,18 +45,32 @@ if __name__ == "__main__":
         print("\n1. Read data from source")
         print("\n2. Write data to target")
         print("\n3. Aggregate data")
-        print("\n4. Exit\n")
+        print("\n4. Join data")
+        print("\n5. Exit\n")
 
         choice=int(input())
         if(choice==1):
             rs = read_source()
+
         elif(choice==2):
             write_target(rs)
+
         elif(choice==3):
             print("\nGroup data by? (Mention Column Name):\n")
             group_choice=input()
             aggregate(group_choice)
             #grouped_data.to_excel(r"C:\Users\saksh\Desktop\Akshansh\Coding\Github\Retail-etl\Excel dataset\P1.xlsx ",sheet_name="Sheet1")
+
+        elif(choice==4):
+            print("\nWhich is the first file?:\n")
+            f1=read_source()
+            print("\nWhich is the second file?:\n")
+            f2=read_source()
+            joiner(f1,f2)
+
+        elif(choice==5):
+            
+
         else:
             quit()
 
